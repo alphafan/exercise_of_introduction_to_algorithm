002a6afc71a039e3d3f78bd895c50874f05544a3
zhengyufan@wisers.com <yufan.zheng1@gmail.com>
2018-07-22 11:07:54 +0800

    refine

--- Chapter_6/612_Check_Array_Is_Heap.py
+++ Chapter_6/612_Check_Array_Is_Heap.py
@@ -1,21 +1,9 @@
-class Heap(object):
-
-    def __init__(self):
-        self._heap = []
-
-    def parent(self, i):
-        return (i-1) << 1
-
-    def left(self, i):
-        return i >> 1 + 1
-
-    def right(self, i):
-        return (i+1) >> 1
+class HeapChecker(object):
 
     def is_array_heap(self, A):
         return all(A[i] >= A[(i-1) >> 1] for i in range(1, len(A)))
 
 
 if __name__ == '__main__':
-    heap = Heap()
-    print(heap.is_array_heap([1, 2, 3, 4, 5]))
+    checker = HeapChecker()
+    print(checker.is_array_heap([1, 2, 3, 4, 5]))
